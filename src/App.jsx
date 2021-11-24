ddb69f546024f3c34a751f056db344cb985d8c3b
Emnauel Rico Conde <ricocondee@gmail.com>
2021-11-24 18:04:13 -0500

    Proyecto en react y bootstrap para sprint2 creado

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import "bootstrap/dist/css/bootstrap.min.css";
+
+const App = () => {
+    return (
+        <div className="col-auto p-4 bg-primary text-center">
+            <h1 className="">Agil Cargo</h1>
+            <p>Estamos en construccion.</p>
+            <br/>
+            <h1 className="text-danger xl">505</h1>
+        </div>
+    );
+}
+
+export default App;
