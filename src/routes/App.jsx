76e04553c694670fca1b384e952a81e742c3c536
Emnauel Rico Conde <ricocondee@gmail.com>
2021-12-09 17:58:48 -0500

    Routes

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -3,8 +3,12 @@ import {BrowserRouter, Switch, Route} from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import Login from '@pages/Login'
+import SignUp from '@pages/SignUp'
 import Home from '@pages/Home'
 import Track from '@pages/Track'
+import Budget from '@pages/Budget'
+import Order from '@pages/Order'
+import Contact from '@pages/Contact'
 import Layout from '@containers/Layout'
 
 const App = () => {
@@ -16,6 +20,9 @@ const App = () => {
                 <Route exact path="/signup" component={SignUp}/>
                 <Route exact path='/login' component={Login} />
                 <Route exact path='/track-shipping' component={Track}/>
+                <Route exact path='/budget' component={Budget}/>
+                <Route exact path='/order' component={Order}/>
+                <Route exact path='/contact' component={Contact}/>
             </Switch>
         </Layout>
     </BrowserRouter>            
